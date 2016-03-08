c36ff3a7517b0c21321658284580c4f9d7a62cae
chirath <chirath.02@gmail.com>
2016-03-09 00:39:58 +0530

    added tests to check student login

--- tests/features/steps.py
+++ tests/features/steps.py
@@ -1,47 +1,14 @@
-from lettuce import world, steps
+from lettuce import *
+from lxml import html
+from selenium import webdriver
+import lettuce_webdriver.webdriver
 
 
 @steps
-class FactorialSteps(object):
-    """Methods in exclude or starting with _ will not be considered as step"""
-
-    exclude = ['set_number', 'get_number']
+class HackademicLoadingtest(object):
+    """Test the loading of Hackademic"""
 
     def __init__(self, environs):
-        self.environs = environs
+        world.browser = webdriver.Firefox()
 
-    def set_number(self, value):
-        self.environs.number = int(value)
-
-    def get_number(self):
-        return self.environs.number
-
-    def _assert_number_is(self, expected, msg="Got %d"):
-        number = self.get_number()
-        assert number == expected, msg % number
-
-    def have_the_number(self, step, number):
-        '''Given I have the number (\d+)'''
-        self.set_number(number)
-
-    def i_compute_its_factorial(self, step):
-        number = self.get_number()
-        self.set_number(factorial(number))
-
-    def check_number(self, step, expected):
-        '''Then I see the number (\d+)'''
-        self._assert_number_is(int(expected))
-
-# Important!
-# Steps are added only when you instanciate the "@steps" decorated class
-# Internally decorator "@steps" build a closure with __init__
-
-FactorialSteps(world)
-
-
-def factorial(number):
-    number = int(number)
-    if (number == 0) or (number == 1):
-        return 1
-    else:
-        return number*factorial(number-1)
+HackademicLoadingtest(world)
--- tests/features/zero.feature
+++ tests/features/zero.feature
@@ -1,17 +1,24 @@
-Feature: Compute factorial
-  In order to play with Lettuce
-  As beginners
-  We'll implement factorial
+Feature: Test if Hackademic loaded
 
-  Scenario Outline: Factorials [0-4]
-    Given I have the number <number>
-    When I compute its factorial
-    Then I see the number <result>
+    Scenario: Simple Hackademic loading test.
+        I go to "http://localhost/hackademic/"
+        I should see "Welcome to Hackademic v0.9!"
+        I should see an element with id of "logo"
 
-  Examples:
-    | number | result |
-    | 0      | 1      |
-    | 1      | 1      |
-    | 2      | 2      |
-    | 3      | 6      |
-    | 4      | 24     |
+    Scenario: Login with Student
+        I fill in "username" with "student"
+        I fill in "pwd" with "123"
+        I press "submit"
+        I should see "Hi student," within 2 seconds
+        I should see "Progress Report"
+        I should see "Ranking"
+        I should see "Challenges"
+        I should not see "Add New Articles"
+        I should not see "Article Manager"
+        I should not see "Users/Classes"
+        I should not see "Add New Challenge"
+        I should not see "Challenge Managers"
+        I should not see "Menu Manager"
+        I should not see "Options"
+        I should not see "Admin Dashboard"
+        I press "menu-item"
